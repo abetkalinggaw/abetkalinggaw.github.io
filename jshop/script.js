39c554ae59b5902ca79f3ca9b3ceafefe2d3fa91
Abet Kalingga Wijaya <75027682+abetkalinggaw@users.noreply.github.com>
2023-03-22 15:31:52 +0700

    Update script.js

--- jshop/script.js
+++ jshop/script.js
@@ -47,10 +47,10 @@ const backTopBtn = document.querySelector("[data-back-top-btn]");
 const activeElemOnScroll = function() {
     if (window.scrollY > 100) {
         header.classList.add("active");
-        backTopBtn.classList.add("active");
+
     } else {
         header.classList.remove("active");
-        backTopBtn.classList.remove("active");
+
     }
 }
 
@@ -105,5 +105,5 @@ function slidefun(n) {
         counter = myslide.length;
     }
     myslide[counter - 1].style.display = "block";
-    dot[counter - 1].className += " active";
+
 }
