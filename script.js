5f7175de6c737a580eac91f736d458de2af3ee7d
Abet Kalingga Wijaya <75027682+abetkalinggaw@users.noreply.github.com>
2023-04-04 10:05:13 +0700

    update menu overlay script

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ sr.reveal('.anim');
 function menuToggle() {
     var nav = document.getElementById('menu-overlay');
     nav.classList.toggle('active');
-    var nav = document.getElementById('toggleIcon');
-    nav.classList.toggle('active');
+    var navIcon = document.getElementById('toggleIcon');
+    navIcon.classList.toggle('active');
 }
+
+function closeMenuOverlay() {
+    var nav = document.getElementById('menu-overlay');
+    nav.classList.remove('active');
+    var navIcon = document.getElementById('toggleIcon');
+    navIcon.classList.remove('active');
+}
+
+var menuLinks = document.querySelectorAll('#menu-overlay a');
+menuLinks.forEach(function(menuLink) {
+    menuLink.addEventListener('click', function() {
+        setTimeout(function() {
+            closeMenuOverlay();
+        }, 500);
+    });
+});
